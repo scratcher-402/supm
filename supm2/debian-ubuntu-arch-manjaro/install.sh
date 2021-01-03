274251f07b6601bb410b7db756f3260b5b3f5cd4
scratcher-402 <75668082+scratcher-402@users.noreply.github.com>
2021-01-03 17:44:34 +0300

    Add files via upload

---
+++ supm2/debian-ubuntu-arch-manjaro/install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+echo Downloading...
+wget https://github.com/scratcher-402/supm/raw/main/supm2/files/duam/supm https://github.com/scratcher-402/supm/raw/main/supm2/files/duam/dis
+echo "1) install dependences with apt-get (debian,ubuntu)
+2) install dependences with pacman (Arch, manjaro)
+3) do not install dependences"
+read a
+case $a in
+1)
+apt-get update
+apt-get install bash
+apt-get install make
+apt-get install build-essential
+apt-get install shc
+;;
+
+2)
+pacman -S bash
+pacman -S make
+pacman -S shc
+;;
+
+esac
+Installing...
+mkdir /usr/share
+mkdir /usr/share/supm
+mkdir /usr/share/supm/pkgs
+cp supm /usr/bin/supm
+rm supm
+chmod 777 /usr/bin/supm
+cp dis /usr/bin/dis
+rm dis
+chmod 777 /usr/bin/dis
+echo https://github.com/scratcher-402/supm-offical-repo/raw/main/ > /usr/share/supm/repo.dis
+echo Done!
+
+
+
+
+
